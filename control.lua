b0e8292d4e7588d2bf20986741bed2aa8b5723d3
klauz <klauzwayne@gmail.com>
2023-11-04 13:08:23 +0100

    Added custom user input

--- control.lua
+++ control.lua
@@ -7,15 +7,20 @@ script.on_event(defines.events.on_built_entity, function(event)
         local rad = game.entity_prototypes[roboportghost.ghost_name].logistic_radius
         if rad then
             rendering.draw_polygon { draw_on_ground = true, color = col, target = roboportghost, surface = player
-            .surface_index, only_in_alt_mode = true, vertices = {
+                .surface_index, only_in_alt_mode = true, vertices = {
                 { target = roboportghost, target_offset = { rad, rad } },
                 { target = roboportghost, target_offset = { -rad, rad } },
                 { target = roboportghost, target_offset = { -rad, -rad } } } }
             rendering.draw_polygon { draw_on_ground = true, color = col, target = roboportghost, surface = player
-            .surface_index, only_in_alt_mode = true, vertices = {
+                .surface_index, only_in_alt_mode = true, vertices = {
                 { target = roboportghost, target_offset = { rad, rad } },
                 { target = roboportghost, target_offset = { rad, -rad } },
                 { target = roboportghost, target_offset = { -rad, -rad } } } }
         end
     end
+end)
+
+script.on_event("toggle-ghost-logistic-range", function(event)
+    settings.get_player_settings(event.player_index)["draw-roboport-ghost-range"] = {
+        value = not settings.get_player_settings(event.player_index)["draw-roboport-ghost-range"].value }
 end)
---
+++ data.lua
@@ -0,0 +1,10 @@
+data:extend({
+    {
+        type = "custom-input",
+        name = "toggle-ghost-logistic-range",
+        localised_name = "Toggle ghost logistic range",
+        consuming = "game-only",
+        key_sequence = "CONTROL + SHIFT + R",
+        action = "lua",
+    }
+})
